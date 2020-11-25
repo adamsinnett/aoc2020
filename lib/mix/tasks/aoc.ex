c4d7fc5ee235b87f0e10488fc0f5509f9c147cb7
Adam Sinnett <quandrum@gmail.com>
2020-11-24 17:54:10 -0800

    Add dynamic dispatch task

---
+++ lib/mix/tasks/aoc.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Aoc do
+  use Mix.Task
+
+  @shortdoc "Run advent of code via mix aoc <day> <part>"
+  def run(args) do
+    [day, part | rest] = args
+
+    input = Input.getInput(2019, day)
+    day = to_string(day) |> String.pad_leading(2, "0")
+    module = String.to_atom("Elixir.AdventOfCode.Day" <> day)
+    partFn = getPartFn(part)
+
+    if (Enum.member?(rest, "-b")) do
+      Benchee.run(%{part_1: fn -> apply(module, partFn, [input]) end})
+    else
+      apply(module, partFn, [input])
+        |> IO.inspect(label: "Results")
+    end
+  end
+
+  defp getPartFn(part) do
+    case part do
+      "1" -> :part1
+      "2" -> :part2
+    end
+  end
+end
