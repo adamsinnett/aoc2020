6d338df9ebbd0763b4885a67fe2cc9a0fb678d81
Adam Sinnett <quandrum@gmail.com>
2020-12-02 22:48:50 -0800

    feat: day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,11 +1,31 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_input) do
+  def part1(input) do
+    position = {3, 1, 0, 0, 0}
+    countTreeAndKnight(position, input)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    positions = [{1, 1, 0, 0, 0}, {3, 1, 0, 0, 0}, {5, 1, 0, 0, 0}, {7, 1, 0, 0, 0}, {1, 2, 0, 0, 0}]
+
+    Enum.map(positions, &countTreeAndKnight(&1, input))
+    |> Enum.reduce(fn x, acc -> x * acc end)
   end
 
   def parseInput(input) do
     String.split(input, "\n", trim: true)
+    |> Enum.map(&String.to_charlist(&1))
+  end
+
+  defp countTreeAndKnight({dx, dy, x, y, count}, input) do
+    line = Enum.at(input, y)
+
+    if line == nil do
+      count
+    else
+      case Enum.at(line, rem(x, length(line))) == 35 do
+        true -> countTreeAndKnight({dx, dy, x + dx, y + dy, count + 1}, input)
+        false -> countTreeAndKnight({dx, dy, x + dx, y + dy, count}, input)
+      end
+    end
   end
 end
