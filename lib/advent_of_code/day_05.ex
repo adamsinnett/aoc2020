a97fc908fc45f1408fb2326ba03008659fc7773b
Adam Sinnett <quandrum@gmail.com>
2020-12-04 23:39:48 -0800

    feat: day 5

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,11 +1,36 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_input) do
+  def part1(input) do
+    Enum.map(input, &resolveSeatNumber(&1))
+    |> Enum.max()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    Enum.map(input, &resolveSeatNumber(&1))
+    |> Enum.sort()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.filter(fn [fir, sec] -> fir + 1 != sec end)
+    |> hd
+    |> hd
+    |> (fn neighbor -> neighbor + 1 end).()
   end
 
   def parseInput(input) do
     String.split(input, "\n", trim: true)
+  end
+
+  defp resolveSeatNumber(ticket) do
+    row =
+      String.slice(ticket, 0, 7)
+      |> String.replace("F", "0")
+      |> String.replace("B", "1")
+      |> String.to_integer(2)
+
+    col =
+      String.slice(ticket, 7, 3)
+      |> String.replace("L", "0")
+      |> String.replace("R", "1")
+      |> String.to_integer(2)
+
+    row * 8 + col
   end
 end
