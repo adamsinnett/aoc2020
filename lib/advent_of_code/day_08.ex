1b0395586464ab5542feb9b27c41303392b4147c
Adam Sinnett <quandrum@gmail.com>
2020-12-07 21:53:17 -0800

    feat: day 8

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,11 +1,67 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_input) do
+  def part1(stack) do
+    {acc, pos} = run(0, stack, 0, [])
+    pos
   end
 
-  def part2(_input) do
+  def part2(stack) do
+    fixStack(stack, 0)
   end
 
   def parseInput(input) do
     String.split(input, "\n", trim: true)
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(fn [op, arg] -> {op, String.to_integer(arg)} end)
+  end
+
+  defp fixStack(stack, idx) do
+
+    {op, arg} = Enum.at(stack, idx)
+    stack = swap(stack, idx, op, arg)
+    {acc, pos} = run(0, stack, 0, [])
+
+    IO.puts("checking #{idx} of #{length(stack)} got #{pos}")
+    cond do
+      pos == length(stack) ->
+        acc
+
+      true ->
+        fixStack(deswap(stack, idx, op, arg), idx + 1)
+    end
+  end
+
+  defp swap(stack, idx, "jmp", arg) do
+    List.replace_at(stack, idx, {"nop", arg})
+  end
+
+  defp swap(stack, idx, "nop", arg) do
+    List.replace_at(stack, idx, {"jmp", arg})
+  end
+
+  defp swap(stack, idx, "acc", arg) do
+    List.replace_at(stack, idx, {"acc", arg})
+  end
+
+  defp deswap(stack, idx, op, arg) do
+    List.replace_at(stack, idx, {op, arg})
+  end
+
+  defp run(pos, stack, acc, history \\ []) do
+    if Enum.member?(history, pos) or pos >= length(stack) do
+      {acc, pos}
+    else
+      {op, arg} = Enum.at(stack, pos)
+
+      case op do
+        "acc" ->
+          run(pos + 1, stack, acc + arg, history ++ [pos])
+
+        "jmp" ->
+          run(pos + arg, stack, acc, history ++ [pos])
+
+        "nop" ->
+          run(pos + 1, stack, acc, history ++ [pos])
+      end
+    end
   end
 end
