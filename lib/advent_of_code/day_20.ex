8a919baf12e1f7389347bda6c952ef24909c9f3d
Adam Sinnett <quandrum@gmail.com>
2020-12-24 10:20:59 -0800

    feat:day 23 part 1

--- lib/advent_of_code/day_20.ex
+++ lib/advent_of_code/day_20.ex
@@ -1,18 +1,80 @@
 defmodule AdventOfCode.Day20 do
-  def part1(_input) do
+  def part1(input) do
+    Enum.map(input, &findNeighbors(&1, input))
+    |> Enum.filter(fn {_, _, _, neighbors} ->
+      2 == Enum.filter(neighbors, &(&1 != nil)) |> length
+    end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.reduce(1, &(&1 * &2))
   end
 
-  def part2(_input) do
+  def part2(input) do
+    tiles = Enum.map(input, &findNeighbors(&1, input))
+
+    _corner =
+      Enum.find(tiles, fn {_, _, _, neighbors} -> 2 == Enum.filter(neighbors, &(&1 != nil)) end)
   end
+
+  # Part 2
+
+  # defp stitch(tiles) do
+  #   tiles
+  #   |> IO.inspect()
+  # end
+
+  # Part 1
+
+  defp findNeighbors({num, pic, {top, right, left, bottom}}, tiles) do
+    north = findMatchingEdge(top, tiles, num)
+    east = findMatchingEdge(right, tiles, num)
+    west = findMatchingEdge(left, tiles, num)
+    south = findMatchingEdge(bottom, tiles, num)
+
+    {num, pic, {top, right, left, bottom}, [north, east, west, south]}
+  end
+
+  defp findMatchingEdge(edge, tiles, curr) do
+    case Enum.find(tiles, fn {num, _, {n, e, w, s}} ->
+           (edge == n or edge == e or edge == w or edge == s or edge == String.reverse(n) or
+              edge == String.reverse(e) or edge == String.reverse(w) or edge == String.reverse(s)) and
+             num != curr
+         end) do
+      {num, _, _} -> num
+      nil -> nil
+    end
+  end
+
+  # Parse
 
   def parseInput(input) do
-    String.split(input, "\n\n", trim: true)
+    String.split(
+      input,
+      "\n\n",
+      trim: true
+    )
     |> Enum.map(&parseTile/1)
-    |> IO.inspect()
   end
 
   defp parseTile("Tile " <> tile) do
-    [num, interior] = String.split(tile, ":")
-    {num, String.split(interior, "\n")}
+    [num, interiorStr] = String.split(tile, ":", trim: true)
+    interiorRows = String.split(interiorStr, "\n", trim: true)
+    {String.to_integer(num), findPicture(interiorRows), findEdges(interiorRows)}
+  end
+
+  defp findPicture(rows) do
+    tl(rows)
+    |> Enum.take(8)
+    |> Enum.map(fn row -> String.graphemes(row) |> tl |> Enum.take(8) end)
+  end
+
+  defp findEdges(rows) do
+    top = hd(rows)
+    bottom = List.last(rows)
+
+    [left, right] =
+      Enum.map(rows, fn row -> {hd(String.graphemes(row)), List.last(String.graphemes(row))} end)
+      |> Enum.reduce(["", ""], fn {ls, rs}, [l, r] -> [l <> ls, r <> rs] end)
+
+    {top, right, left, bottom}
   end
 end
--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -1,11 +1,54 @@
 defmodule AdventOfCode.Day23 do
-  def part1(_input) do
+  def part1(cups) do
+    finished = playGame(cups, 0, 100)
+
+    #find one and rotate around it, but not include it
+    one = Enum.find_index(finished, &(&1 == 1))
+    Enum.drop(finished, one+1) ++ Enum.take(finished, one)
+    |> Enum.join("")
   end
 
   def part2(_input) do
   end
 
-  def parseInput(input) do
-    String.split(input, "\n", trim: true)
+  # PART 1 - Too much list - manual entry
+
+  defp playGame(cups, current, move) when current == length(cups), do: playGame(cups, 0, move)
+  defp playGame(cups, _, move) when move == 0, do: cups
+
+  defp playGame(cups, currindex, move) do
+    # find game pieces
+    current = Enum.at(cups, currindex)
+    pickup = sliceOutThreeAt(cups, currindex + 1)
+    sliced = Enum.filter(cups, fn i -> not Enum.member?(pickup, i) end)
+    destination = findDestination(current, sliced)
+    # splice
+    di = Enum.find_index(sliced, &(&1 == destination))
+    updated = Enum.take(sliced, di + 1) ++ pickup ++ Enum.drop(sliced, di + 1)
+
+    playGame(updated, Enum.find_index(updated, &(&1 == current)) + 1, move - 1)
   end
+
+  defp sliceOutThreeAt(cups, index, count \\ 0)
+
+  defp sliceOutThreeAt(cups, index, count) when index == length(cups),
+    do: sliceOutThreeAt(cups, 0, count)
+
+  defp sliceOutThreeAt(_, _, count) when count == 3, do: []
+
+  defp sliceOutThreeAt(cups, index, count) do
+    [Enum.at(cups, index)] ++ sliceOutThreeAt(cups, index + 1, count + 1)
+  end
+
+  defp findDestination(current, sliced) when current < 1, do: findDestination(10, sliced)
+
+  defp findDestination(current, sliced) do
+    case Enum.member?(sliced, current - 1) do
+      true -> current - 1
+      false -> findDestination(current - 1, sliced)
+    end
+  end
+
+  def parseInput(input),
+    do: String.trim(input) |> String.graphemes() |> Enum.map(&String.to_integer/1)
 end
